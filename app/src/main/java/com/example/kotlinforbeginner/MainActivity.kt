e92327f4b5350d59e250a1daa90f4590624de93b
Alexsander Faria <alexsander.itj@hotmail.com>
2022-06-17 21:28:25 -0300

    teste de requisicao http get com kotlin

--- app/src/main/java/com/example/kotlinforbeginner/MainActivity.kt
+++ app/src/main/java/com/example/kotlinforbeginner/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.kotlinforbeginner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.util.JsonReader
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+import java.net.URL
+import kotlin.math.log
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val client = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,5 +30,23 @@ class MainActivity : AppCompatActivity() {
         btn.setOnClickListener{
             Toast.makeText(this@MainActivity, "Clicado", Toast.LENGTH_SHORT).show()
         }
+
+
+        //teste http request | a principio deu certo
+        run("https://rickandmortyapi.com/api/character/2")
+    }
+
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+        var resposta : String = ""
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) { resposta ="deu errado" }
+            override fun onResponse(call: Call, response: Response) { resposta = response.body!!.string()}
+        })
+        println(resposta)
+        val per = JSONObject(resposta)
+        val s = ""
     }
 }
